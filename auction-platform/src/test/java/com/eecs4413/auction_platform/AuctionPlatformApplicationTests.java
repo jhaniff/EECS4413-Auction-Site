69eb8fa9e51a2bf6b6fb03d3e0f3928528acd4a9
jhaniff <90522357+jhaniff@users.noreply.github.com>
2025-10-08 20:05:09 -0400

    feat: initialize frontend with Vite and React setup
    - Added package.json with dependencies for React, Vite, and development tools.
    - Included Vite logo SVG in public directory.
    - Created App.css for styling the application.
    - Developed App.tsx as the main component with state management.
    - Added React logo SVG in assets directory.
    - Established global styles in index.css.
    - Set up main.tsx for rendering the application.
    - Configured TypeScript with tsconfig.app.json and tsconfig.node.json for frontend and node environments.
    - Created vite.config.ts for Vite configuration.

---
+++ auction-platform/src/test/java/com/eecs4413/auction_platform/AuctionPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eecs4413.auction_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
