61fac64c15f69cb5de705caa3ff7ec68f049758f
George <george_zakko@hotmail.com>
2025-10-25 11:41:09 -0400

    Created the DTO for payment requests.  Fields are all derived from the project description.

---
+++ auction-platform/src/main/java/com/eecs4413/auction_platform/dto/PaymentRequestDTO.java
@@ -0,0 +1,29 @@
+package com.eecs4413.auction_platform.dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentRequestDTO {
+     private Long paymentID;
+     private String firstName;
+     private String lastName;
+     private String streetName;
+     private String streetNumber;
+     private String city;
+     private String country;
+     private String postalCode;
+     private Long cardNumber;
+     private String nameOnCard;
+     private Date expiryDate;
+     private Long securityCode;
+}
