3b1673ed0e76685d09c0efa41b0f1cb8be2976d1
George <george_zakko@hotmail.com>
2025-10-25 19:11:03 -0400

    Created a basic receipt response DTO. I felt like a receipt request DTO wasn't necessary because most of the data for a receipt is derived in payment.

---
+++ auction-platform/src/main/java/com/eecs4413/auction_platform/dto/ReceiptResponseDTO.java
@@ -0,0 +1,16 @@
+package com.eecs4413.auction_platform.dto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ReceiptResponseDTO {
+
+}
