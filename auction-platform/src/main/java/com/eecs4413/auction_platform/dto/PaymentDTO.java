f9e36911e1c2942003875f2c11f1651f2ea6f34b
George <george_zakko@hotmail.com>
2025-10-30 18:00:53 -0400

    Added the required fields that make up a payment to the PaymentDTO class.

--- auction-platform/src/main/java/com/eecs4413/auction_platform/dto/PaymentDTO.java
+++ auction-platform/src/main/java/com/eecs4413/auction_platform/dto/PaymentDTO.java
@@ -1,4 +1,23 @@
 package com.eecs4413.auction_platform.dto;
 
+import com.eecs4413.auction_platform.model.Auction;
+import com.eecs4413.auction_platform.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PaymentDTO {
+     private Long paymentID;
+     private Auction auction;
+     private User payee;
+     private OffsetDateTime paymentDate;
+     private OffsetDateTime expectedDeliveryDate;
+
 }
