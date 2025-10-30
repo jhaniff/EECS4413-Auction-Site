d691135e832c45eb49dbb197725f4a1dd9bd7e83
George <george_zakko@hotmail.com>
2025-10-30 18:30:33 -0400

    Removed unnecessary imports. Also modified the name of the ony function in the class from getAuction to getPayment - naming remnant from another method in the PaymentController class.

--- auction-platform/src/main/java/com/eecs4413/auction_platform/controller/PaymentController.java
+++ auction-platform/src/main/java/com/eecs4413/auction_platform/controller/PaymentController.java
@@ -1,14 +1,6 @@
 package com.eecs4413.auction_platform.controller;
-import com.eecs4413.auction_platform.dto.AuctionDTO;
-import com.eecs4413.auction_platform.dto.AuctionDetailDTO;
-import com.eecs4413.auction_platform.dto.PaymentDTO;
 import com.eecs4413.auction_platform.dto.PaymentDetailDTO;
-import com.eecs4413.auction_platform.service.AuctionService;
 import com.eecs4413.auction_platform.service.PaymentService;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,7 +14,7 @@ public class PaymentController {
      }
 
      @GetMapping("/{paymentId}")
-     public ResponseEntity<PaymentDetailDTO> getAuction(@PathVariable Long paymentId){
+     public ResponseEntity<PaymentDetailDTO> getPayment(@PathVariable Long paymentId){
           return ResponseEntity.ok(paymentService.getPaymentDetails(paymentId));
      }
 
