0f86d277ee900686b6fd70039304c927ae6c1bf0
George <george_zakko@hotmail.com>
2025-10-30 17:54:15 -0400

    Created a simple PaymentController class to again, match the overall architecture of the project. This class currently finds the payment details of a winning bidder.

--- auction-platform/src/main/java/com/eecs4413/auction_platform/controller/PaymentController.java
+++ auction-platform/src/main/java/com/eecs4413/auction_platform/controller/PaymentController.java
@@ -1,4 +1,29 @@
 package com.eecs4413.auction_platform.controller;
+import com.eecs4413.auction_platform.dto.AuctionDTO;
+import com.eecs4413.auction_platform.dto.AuctionDetailDTO;
+import com.eecs4413.auction_platform.dto.PaymentDTO;
+import com.eecs4413.auction_platform.dto.PaymentDetailDTO;
+import com.eecs4413.auction_platform.service.AuctionService;
+import com.eecs4413.auction_platform.service.PaymentService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/auction")
 public class PaymentController {
+     private final PaymentService paymentService;
+
+     public PaymentController(PaymentService paymentService){
+          this.paymentService = paymentService;
+     }
+
+     @GetMapping("/{paymentId}")
+     public ResponseEntity<PaymentDetailDTO> getAuction(@PathVariable Long paymentId){
+          return ResponseEntity.ok(paymentService.getPaymentDetails(paymentId));
+     }
+
 }
