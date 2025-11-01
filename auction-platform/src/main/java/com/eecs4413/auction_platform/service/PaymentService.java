b9dccefbbad341698534e448a1ab07e80a68fbf6
George <george_zakko@hotmail.com>
2025-11-01 18:18:26 -0400

    Added annotation for Service on the PaymentService class.

--- auction-platform/src/main/java/com/eecs4413/auction_platform/service/PaymentService.java
+++ auction-platform/src/main/java/com/eecs4413/auction_platform/service/PaymentService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
+@Service
 public class PaymentService {
      PaymentRepository paymentRepository;
      AuctionRepository auctionRepository;
