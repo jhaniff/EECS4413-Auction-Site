2ba31a835529a8904743387301b6de56ebe82bd5
George <george_zakko@hotmail.com>
2025-11-02 10:57:36 -0500

    Got rid of improper import.

--- auction-platform/src/main/java/com/eecs4413/auction_platform/dto/PaymentRequestDTO.java
+++ auction-platform/src/main/java/com/eecs4413/auction_platform/dto/PaymentRequestDTO.java
@@ -1,7 +1,6 @@
 package com.eecs4413.auction_platform.dto;
 
 import com.eecs4413.auction_platform.model.User;
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
