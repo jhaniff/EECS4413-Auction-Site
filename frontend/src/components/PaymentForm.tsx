adb7710cd1b586f7f477318ee02789a9a2f68ec7
George <george_zakko@hotmail.com>
2025-11-16 14:42:14 -0500

    This is all the payment form with the correct fields.

--- frontend/src/components/PaymentForm.tsx
+++ frontend/src/components/PaymentForm.tsx
@@ -8,3 +8,51 @@ function PaymentForm({onSubmitPayment}) {
       expiryDate,
       securityCode
     };
+
+      function handleChange(e) {
+        setPayment(e.target.value);
+      }
+
+      function handleSubmit(e) {
+        e.preventDefault();
+        onSubmitPayment(paymentInfo);
+      }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Card Number:
+        <input
+            type="number"
+            value={cardNumber}
+            onChange={handleChange}
+         />
+      </label>
+      <label>Name on Card:
+              <input
+                  type="text"
+                  value={nameOnCard}
+                  onChange={handleChange}
+               />
+            </label>
+      <label>:Expiry Date:
+              <input
+                  type="text"
+                  value={expiryDate}
+                  onChange={handleChange}
+               />
+            </label>
+      <label>Security Code:
+              <input
+                  type="number"
+                  value={securityCode}
+                  onChange={handleChange}
+               />
+            </label>
+      <input type="submit" />
+          {<p>Your card number is: {cardNumber}.  </p>}
+          {<p>Your name on card is: {nameOnCard}.  </p>}
+          {<p>Your expiry date is: {expiryDate}.  </p>}
+          {<p>Your security code is: {securityCode}.  </p>}
+    </form>
+  )
+}
+export default PaymentForm;
