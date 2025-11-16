331b03d2d12443728b3c05e5bf843fbc8de87f3e
George <george_zakko@hotmail.com>
2025-11-16 15:12:08 -0500

    Added a for the payment form, and print basic testing messages for correct payment field input.

---
+++ frontend/src/pages/PaymentPage.tsx
@@ -0,0 +1,19 @@
+import PaymentForm from "../components/PaymentForm";
+import { useParams } from "react-router-dom";
+
+function PaymentPage(){
+      function handlePayment(paymentInfo){
+            console.log("Card Number:", paymentInfo.cardNumber);
+            console.log("Name on Card:", paymentInfo.nameOnCard);
+            console.log("Expiry Date:", paymentInfo.expiryDate);
+            console.log("Security Code:", paymentInfo.securityCode);
+          }
+
+    return(
+        <div>
+            <h2>Winning Bidder</h2>
+            <PaymentForm onSubmitPayment={handlePayment}/>
+        </div>
+        )
+    }
+export default PaymentPage;
