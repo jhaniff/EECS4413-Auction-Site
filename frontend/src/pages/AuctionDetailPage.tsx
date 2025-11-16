2955b962b9a0483ec2e63dee2d9cb22511517f19
George <george_zakko@hotmail.com>
2025-11-15 20:37:06 -0500

    Implemented the AuctionDetailsPage function which gets the correct auction ID and bid amount from a client form.

---
+++ frontend/src/pages/AuctionDetailPage.tsx
@@ -0,0 +1,20 @@
+import BidForm from "../components/BidForm";
+import { useParams } from "react-router-dom";
+
+function AuctionDetailPage(){
+      const { id } = useParams();
+      function handleBid(bidAmount: string){
+            console.log("Submitting bid...");
+            console.log("Auction ID:", id);
+            console.log("Bid amount:", bidAmount);
+          }
+
+    return(
+        <div>
+            <h2> Auction Details</h2>
+            <p>Auction ID: {id}</p>
+            <BidForm onSubmitBid={handleBid}/>
+        </div>
+        )
+    }
+export default AuctionDetailPage;
