2dccc92f2da58c4274835f1aab486919a206bc1d
George <george_zakko@hotmail.com>
2025-11-16 20:32:50 -0500

    Created the response.

--- frontend/src/api/paymentAPI.ts
+++ frontend/src/api/paymentAPI.ts
@@ -1,5 +1,9 @@
 
 export async function placePayment(paymentInfo){
     const URL = "http://localhost:8080/payment/place";
-    
+    const response = await fetch(URL, {
+        method: "POST",
+        heads: {"Content-Type": "application/json"},
+        body: JSON.stringify(paymentInfo)
+        });
 }
