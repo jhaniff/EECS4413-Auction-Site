9d21b81938b6ba56bd618745d861542f77a23ced
George <george_zakko@hotmail.com>
2025-11-16 20:34:35 -0500

    Typo: changed 'heads' to 'headers'.

--- frontend/src/api/paymentAPI.ts
+++ frontend/src/api/paymentAPI.ts
@@ -3,7 +3,7 @@ export async function placePayment(paymentInfo){
     const URL = "http://localhost:8080/payment/place";
     const response = await fetch(URL, {
         method: "POST",
-        heads: {"Content-Type": "application/json"},
+        headers: {"Content-Type": "application/json"},
         body: JSON.stringify(paymentInfo)
         });
     return response.json();
