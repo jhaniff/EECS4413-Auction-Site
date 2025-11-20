baa1ac1f86923b12153a1f662d3a030299d7889d
George <george_zakko@hotmail.com>
2025-11-20 10:50:44 -0500

    Simplified print statements to display full payment payload.

--- frontend/src/pages/PaymentPage.tsx
+++ frontend/src/pages/PaymentPage.tsx
@@ -14,11 +14,8 @@ function PaymentPage(){
                securityCode: paymentInfo.securityCode,
                isExpedited: paymentInfo.isExpedited
            };
-           console.log("Card Number:", paymentInfo.cardNumber);
-           console.log("Name on Card:", paymentInfo.nameOnCard);
-           console.log("Expiry Date:", paymentInfo.expiryDate);
-           console.log("Security Code:", paymentInfo.securityCode);
-           console.log("Shipping Status:", paymentInfo.expeditedShipping);
+           console.log("Payment Payload:", fullPaymentPayload);
+
     return(
         <div>
             <h2>Winning Bidder</h2>
