90e52f0dda0a70c29de5b81d7a35d3b19795bd94
George <george_zakko@hotmail.com>
2025-11-24 18:17:13 -0500

    Just copy and pasted useEffect from payment page and revamped it.

--- frontend/src/pages/AuctionDetailPage.tsx
+++ frontend/src/pages/AuctionDetailPage.tsx
@@ -6,6 +6,12 @@ function AuctionDetailPage(){
       const { id } = useParams();
       const [auction, setAuction] = useState(null);
 
+      useEffect(() => {
+          fetch(`http://localhost:8080/auction/${id}`)
+           .then(resp => resp.json())
+           .then(data => setAuction(data));
+      }, [id]);
+
     return(
         <div>
             <h2> Auction Details</h2>
